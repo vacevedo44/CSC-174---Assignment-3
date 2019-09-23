1be5219253fb9e6954b502f6d001b2bc54d2900b
anels28 <55111942+anels28@users.noreply.github.com>
2019-09-23 16:57:45 -0400

    index.php file, home page set up

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <title> Home | Assignment 3: Information Architecture</title>
+      <link rel="stylesheet" href="css/styles.css">
+   </head>
+
+   <body class="container">
+
+      <header>
+         <a href="index.php">
+         <img href="index.php" src="images/dms.jpg" alt="computer">
+         </a>
+      </header>
+
+      <main>
+         <h1>Welcome to the DMS Majors</h1>
+         <p>
+            This is our Home Page for Assignment 3: Information Architecture
+            <br> Our Team's Roles:
+            <br> Alyssa: Information Architect 
+            <br> Vaibhav: Visual Designer 
+            <br> Vanessa: Tech Coder
+         </p>
+      </main>
+
+      <script src="http://code.jquery.com/jquery.js"></script>
+      <script src="js/menu-highlighter.js"></script>	
+
+   </body>
+
+</html>
