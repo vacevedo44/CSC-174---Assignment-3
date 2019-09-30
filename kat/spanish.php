277ce19ee34b9dae6ee231c25bd5776ec02b2aad
anels28 <55111942+anels28@users.noreply.github.com>
2019-09-30 09:53:10 -0400

    fixing menu title

--- kat/spanish.php
+++ kat/spanish.php
@@ -35,7 +35,7 @@
 
    <main>
       
-      <h1 class="main-heading">Spanish</h1>
+      <h1 class="main-heading">Academia</h1>
       <div class="main-text" >
             <p>Last year, I decided to pursue a minor in Spanish, and I will be taking another course to continue that this semester. When I first came to college, I started taking German, but after one semester I decided that it wasn’t for me, and chose to switch back to Spanish, which I had taken in high school. I now have three courses left to fulfill the minor, including the one I am taking this fall, and I am hoping to finish the last one through a summer program in Ecuador. The program runs for four weeks, and consists of classes during the day, and various trips and excursions on the weekends. This trip seems like an excellent way to experience another culture, practice different language skills, and, as someone who has lived in Rochester her whole life, serve as a much needed change of scenery.</p>
 
